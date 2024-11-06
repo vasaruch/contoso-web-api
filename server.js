3865b70cf8a23565c48f68639c4b6bf30ad03940
Vasaruch R <vasarucr@gmail.com>
2024-11-06 08:47:03 +0000

    Initial WebApp

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8000;
 const HOST = '0.0.0.0';
 const app = express();
 app.get('/',(req,res) => {
-    res.send('Hello World - 1.0.0.0');
+    res.send('Hello World - 2.0.0.0');
 });
 app.listen(PORT,HOST,() => {
     console.log(`Running On http://${HOST}:${PORT}`);
